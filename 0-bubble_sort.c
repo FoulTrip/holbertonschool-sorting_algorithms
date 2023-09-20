29c6e17ee01431dd0cf2784a1c78200cb74b60a7
foultrip <davidvasquezmahecha@gmail.com>
2023-09-20 08:56:34 -0500

    act001.1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
         {
 		swapped = 0;
-		
+
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -32,6 +32,8 @@ void bubble_sort(int *array, size_t size)
 		}
 
 		if (swapped == 0)
+		{
 			break;
-    }
+		}
+	}
 }
