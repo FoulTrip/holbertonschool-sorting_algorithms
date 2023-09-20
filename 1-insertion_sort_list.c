a15418e96a9e104f1bdcd42b13c5aa041ef39cc6
foultrip <davidvasquezmahecha@gmail.com>
2023-09-20 10:32:53 -0500

    act002.4

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *prev, *next;
-	
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	
@@ -22,9 +22,12 @@ void insertion_sort_list(listint_t **list)
 		while (prev != NULL && prev->n > current->n)
 		{
 			if (current->next != NULL)
+				prev->prev->next = current;
+
+			if (current->next != NULL)
 				current->next->prev = prev;
 			
-			prev->next = next;
+			prev->next = current->next;
 			current->prev = prev->prev;
 			current->next = prev;
 			prev->prev = current;
@@ -34,12 +37,7 @@ void insertion_sort_list(listint_t **list)
 			else
 				current->prev->next = current;
 
-			if (next != NULL)
-				next->prev = prev;
-
 			prev = current->prev;
-
-			print_list(*list);
 		}
 
 		current = next;
