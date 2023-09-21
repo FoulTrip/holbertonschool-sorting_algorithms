a40cb10e499b322a628aaf1cd38663db114b7958
foultrip <davidvasquezmahecha@gmail.com>
2023-09-21 10:48:51 -0500

    act003.2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,40 +1,47 @@
 #include "sort.h"
 
 /**
- * selection_sort - sort an array using a selection sort algorithm
- * @array: pointer to an array
- * @size: sie of the array
-*/
+ * selection_sort - Calls function
+ * @array: Array to be sorted
+ * @size: Size of array given
+ * Descrtiption: Function that sorts an array using the Selection
+ * sort algotrithm
+ * Return: 0
+ */
 void selection_sort(int *array, size_t size)
 {
-	int iterator, tmp, minValue;
-	size_t i, minPosition = 0;
+	unsigned int i, j, min_idx;
 
-	iterator = tmp = minValue = 0;
-
-	if (!array || size == 0)
+	if (!array)
 		return;
 
-	while (iterator < (int) size - 1)
+	for (i = 0; i < size - 1; i++)
 	{
-		tmp = minValue = array[iterator];
-		for (i = iterator; i < size; i++)
-		{
-			if (array[i] < minValue)
-			{
-				minValue = array[i];
-				minPosition = i;
-			}
-		}
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
 
-		if (minValue < array[iterator])
+		if (i != min_idx)
 		{
-			array[iterator] = minValue;
-			array[iterator] = tmp;
+			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
 		}
-
-		iterator++;
 	}
-	
+}
+
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
 }
